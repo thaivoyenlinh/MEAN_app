baf60011ce1ad04af9de2df0f79be20955ac86ff
Thaivoyenlinh <thaivoyenlinh007@gmail.com>
2021-07-15 02:01:12 +0700

    create UI's Category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//? Components
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { SignUpComponent } from './components/authentication/sign-up/sign-up.component';
 import { SignInComponent } from './components/authentication/sign-in/sign-in.component';
 import { AdminComponent } from './components/admin/admin.component';
-import { ReactiveFormsModule, FormsModule} from '@angular/forms';
-
-// ?import to use Material Component
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-// import {MaterialFileInputModule} from 'ngx-material-file-input';
-import {MatButtonModule} from '@angular/material/button';
-
-//? import to use Sidenav
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import {MatDividerModule} from '@angular/material/divider';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CategoryComponent } from './components/category/category.component';
 import { UserComponent } from './components/user/user.component';
 import { ItemComponent } from './components/item/item.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// ?import to use Material Component for UI's user
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+// import {MaterialFileInputModule} from 'ngx-material-file-input';
+import {MatButtonModule} from '@angular/material/button';
+
+//? import for UI's admin
+//* use Sidenav
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+//* use table
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -62,6 +67,7 @@ import { ItemComponent } from './components/item/item.component';
     MatToolbarModule,
     MatSidenavModule,
     MatDividerModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  symbol: string;
+  
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', symbol: 'H'},
+  {position: 2, name: 'Helium', symbol: 'He'},
+  {position: 3, name: 'Lithium', symbol: 'Li'},
+];
+
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -7,9 +22,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
+  displayedColumns: string[] = ['position', 'name', 'symbol', 'action'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
